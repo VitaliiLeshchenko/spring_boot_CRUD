fbc26e6709453816b038742861787f0b98cbb436
Vitalii <59371243+VitaliiLeshchenko@users.noreply.github.com>
2020-07-27 13:53:40 +0300

    olo

--- src/main/java/vitalii/leshchenko/spring_boot_CRUD/controller/UserController.java
+++ src/main/java/vitalii/leshchenko/spring_boot_CRUD/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import vitalii.leshchenko.spring_boot_CRUD.model.User;
 import vitalii.leshchenko.spring_boot_CRUD.service.UserService;
 
@@ -24,5 +26,35 @@ public class UserController {
         List<User> userList = userService.getAll();
         model.addAllAttributes(Map.of("users", userList));
         return "user-list";
+    }
+
+    @GetMapping("/user-create")
+    public String createUserForm(User user) {
+        return "user-create";
+    }
+
+    @PostMapping("/user-create")
+    public String createUser(User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-delete/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/user-update/{id}")
+    public String updateForm(@PathVariable("id") long id, Model model) {
+        User user = userService.findById(id);
+        model.addAllAttributes(Map.of("user", user));
+        return "user-update";
+    }
+
+    @PostMapping("/user-update")
+    public String updateUser(User user) {
+        userService.save(user);
+        return "redirect:/users";
     }
 }
--- src/main/java/vitalii/leshchenko/spring_boot_CRUD/model/User.java
+++ src/main/java/vitalii/leshchenko/spring_boot_CRUD/model/User.java
@@ -15,7 +15,7 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "first_name")
